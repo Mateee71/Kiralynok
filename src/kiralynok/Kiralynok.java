18f5d71a28127438708e6cfb87ae3a24d01f3ae4
MateKovacs <netmate12@gmail.com>
2021-02-18 10:22:08 +0100

    4. Feladat kész

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 public class Kiralynok {
     public static void main(String[] args) throws IOException {
         
+        System.out.println("4. feldat: üres tábla megjelenítése:");
+        Tabla tabla = new Tabla('#');
+        tabla.megjelenit();
     }
     
 }
